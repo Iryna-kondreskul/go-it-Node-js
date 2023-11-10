39fefd87a0782ed3b43a9b1336373467634e47f1
Iryna-kondreskul <74777086+Iryna-kondreskul@users.noreply.github.com>
2023-11-10 15:47:35 +0200

    .

---
+++ contacts.js
@@ -0,0 +1,35 @@
+const fs = require("fs").promises;
+const path  = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+// contacts.js
+
+/*
+ * Раскомментируй и запиши значение
+const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+    // ...твой код. Возвращает массив контактов.
+  }
+  
+  function getContactById(contactId) {
+    // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
+  }
+  
+  function removeContact(contactId) {
+    // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твой код. Возвращает объект добавленного контакта. 
+  }
+
+ module.export = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+ } 
